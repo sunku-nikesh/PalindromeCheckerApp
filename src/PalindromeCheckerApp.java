207fdeed252143135b5c446c3e256443ca63db15
Viggiji <vighneshgarg96@gmail.com>
2026-03-06 16:09:34 +0530

    UC3 string reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        String input = "level";
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
         String reversed = "";
+
+        // Reverse string
         for (int i = input.length() - 1; i >= 0; i--) {
             reversed = reversed + input.charAt(i);
         }
-        boolean isPalindrome = input.equals(reversed);
-        System.out.println("Input text: " + input);
-        System.out.println("Reversed text: " + reversed);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        // Check palindrome
+        if (input.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
     }
-}
+    }
